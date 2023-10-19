b55d29792c600e3419e244416546e6f806bcefd1
panchitowok2 <francisco.torres@est.fi.uncoma.edu.ar>
2023-10-19 17:15:26 -0400

    Agregamos el header y el footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='position-absolute bottom-0 bg-primary w-100'>
+            <div>
+                <p className='text-center'>Pie de página de la página</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='bg-info w-100'>
+        <div>
+        <h1>Hospital Conrado Villalba</h1>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
