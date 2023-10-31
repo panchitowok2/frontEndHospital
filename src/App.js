91968c4076a47797a8852ff919fdcfd3ec631ac5
Stefano <stefanofabi96@gmail.com>
2023-10-31 16:07:11 -0300

    Corregir el footer para que quede siempre al final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from './components/Footer/Footer.jsx';
 import Content from './components/Content/Content.jsx'
 function App() {
   return (
-    <div className="container-fluid position-relative vh-100 vw-100 m-0 p-0">
+    <div className="container-fluid position-relative min-vh-100 vw-100 m-0 p-0">
       <Header />
       <Content />
       <Footer />
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,9 +3,29 @@ import React from 'react';
 import Perfil from '../Perfil/Perfil.jsx'
 function Content() {
   return (
-        <div>
+        <div className="footerSiempreAbajo">
         <h1>Hospital Conrado Villalba</h1>
         <Perfil />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        asdasd <br />
+        Final <br />
         </div>
   );
 }
